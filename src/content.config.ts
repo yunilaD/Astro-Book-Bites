4c2a0b9478b37a3a007ab1fbbab3590175c3bd93
yunilaD <yunilad2006@gmail.com>
2026-07-13 14:42:07 +0530

    feat: add BookCard component and define books content collection schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,10 @@
-import { file } from "astro/loaders"
+import { glob } from "astro/loaders"
 import { defineCollection } from "astro:content"
 import { z } from "astro/zod"
 
 const books = defineCollection({
-    loader: file('src/content/books.json'),
+    loader: glob({ base: 'src/content/books', pattern: '*.md' }),
     schema: z.object({
-        id: z.string(),
         title: z.string(),
         author: z.string(),
         rating: z.number(),
